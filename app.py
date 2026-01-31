d1653ee51c99337e55453c300932b1fcc6ec9964
plumlariat <ericjeffcoat85@gmail.com>
2026-01-31 13:16:31 -0800

    created data structure for read-in text. Almost finished functionality for main word display.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import sys, random
+import sys
 
 from PySide6.QtWidgets import (
     QApplication,
@@ -14,18 +14,8 @@ from PySide6.QtWidgets import (
     QGridLayout
 )
 from PySide6.QtGui import QIcon, QAction
-from PySide6 import QtCore
-
-from layout_colorwidget import Color
-
-def txt_file_to_list(filepath: str) -> list[str]:
-    
-    with open(filepath) as file:
-        f_str = file.read()
-        token_list = f_str.split()
-
-    return token_list
-
+from PySide6.QtCore import QTimer, Slot, Qt
+from word_list import WordList
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -34,30 +24,47 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(QIcon('./assets/lightning.png'))
         self.resize(800, 600)
 
-        # define all necessary components
+        # Non-GUI application variables
+        self.word_list = WordList()
 
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer_tick)
+        self.timer_interval = 200
+
+        # define and setup all necessary components 
         self.word_display = QLabel("Upload a file to get started.")
-        self.pb = QProgressBar()
+        self.word_display_font = self.word_display.font()
+        self.word_display_font.setPointSize(32)
+        self.word_display.setFont(self.word_display_font)
+
+        self.prog_bar = QProgressBar()
+        self.prog_bar.setMinimum(0)
+        self.prog_bar.setTextVisible(False)
+
         self.wpm_control = QSpinBox()
+        self.wpm_control.setRange(1,500)
+        self.wpm_control.setValue(self.timer_interval)
+        self.wpm_control.setSingleStep(10)
+        self.wpm_control.valueChanged.connect(self.wpm_changed)
+
+
         self.play_button = QPushButton("Play")
         self.pause_button = QPushButton("Pause")
-        self.upload_file_button = QPushButton("Upload Text")
 
         # connect signals and slots and other set-up vars
         self.play_button.clicked.connect(self.play)
         self.pause_button.clicked.connect(self.pause)
-        self.upload_file_button.clicked.connect(self.uploadFile)
 
-        # disable controls without file upload
+        # disable controls before a file upload
         self.play_button.setEnabled(False)
         self.pause_button.setEnabled(False)
 
-        # Define the layout
+        # Define the layout, place components
         layout = QGridLayout()
-        layout.addWidget(self.word_display, 0, 0, 4, 7, alignment=QtCore.Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.word_display, 0, 0, 4, 7, alignment=Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(self.play_button, 4, 0)
         layout.addWidget(self.pause_button, 4, 1)
-        layout.addWidget(self.pb, 4, 2, 1, 3)
+        layout.addWidget(self.prog_bar, 4, 2, 1, 3)
         layout.addWidget(self.wpm_control, 4, 5, 1, 2)
 
         widget = QWidget()
@@ -65,16 +72,38 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(widget)
         self.setMenuBar(self.__init_menu_bar())
 
-    @QtCore.Slot()
+    @Slot()
     def play(self):
-        print("I should play now")
+        if self.word_list.complete:
+            self.word_list.reset()
+            self.prog_bar.setValue(self.word_list.cur_index)
+            self.word_display.setText(self.word_list.get_current_word())
 
-    @QtCore.Slot()
+        self.play_button.setEnabled(False)
+        self.timer.setInterval(self.timer_interval) # 1 second intervals
+        self.timer.start()
+
+    @Slot()
     def pause(self):
-        print("I should pause now")
+        self.play_button.setEnabled(True)
+        self.timer.stop()
 
-    @QtCore.Slot()
-    def uploadFile(self):
+    @Slot()
+    def timer_tick(self):
+        if not self.word_list.complete:
+            self.word_list.next()
+            self.word_display.setText(self.word_list.get_current_word())
+            self.prog_bar.setValue(self.word_list.cur_index)
+        else:
+            self.pause()
+
+
+    @Slot(int)
+    def wpm_changed(self, value: int) -> int:
+        # go from wpm to milisecond
+        self.timer_interval
+
+    def upload_file_action(self):
         f_name, _ = QFileDialog.getOpenFileName(
             self,
             "Open Text File",
@@ -83,16 +112,23 @@ class MainWindow(QMainWindow):
         )
 
         if f_name:
-            self.text.setText(f"Selected file: {f_name}.")
-            t_list = txt_file_to_list(f_name)
-            print(t_list)
+            self.word_list = WordList()
+            self.word_list.init_from_txt_file(f_name)
+            self.word_display.setText(self.word_list.get_current_word())
             self.play_button.setEnabled(True)
             self.pause_button.setEnabled(True)
+            self.prog_bar.setMaximum(self.word_list.remaining)
     
     def __init_menu_bar(self) -> QMenuBar:
         mb = QMenuBar()
         
-        fm = QMenu(title="File")
+        fm = QMenu("File", self)
+        open_action: QAction = QAction("Open", self)
+        open_action.setShortcut("Ctrl+O")
+        open_action.triggered.connect(self.upload_file_action)
+        fm.addAction(open_action) # type: ignore
+
+        
 
         em = QMenu(title="Edit")
         
@@ -103,6 +139,12 @@ class MainWindow(QMainWindow):
         mb.addMenu(hm)
 
         return mb
+    
+def wpm_to_ms(words: int) -> int:
+    '''Get the time in microseconds required to display one word at requested words per minute'''
+    return int(60_000.0 / words)
+
+
 
 
 if __name__ == "__main__":
---
+++ word_list.py
@@ -0,0 +1,48 @@
+class WordList:
+    def __init__(self) -> None:
+        self.l = []
+        self.cur_index = 0
+        self.remaining = 0
+        self.complete = False
+
+    def init_from_txt_file(self, txt_file_path) -> None:
+        self.l = self.__txt_file_to_list(txt_file_path)
+        self.remaining = len(self.l) - 1
+        #print(self.l)
+
+    def reset(self) -> None:
+        self.cur_index = 0
+        self.remaining = len(self.l) - 1
+        self.complete = False
+
+    def get_current_word(self) -> str:
+        return self.l[self.cur_index]
+    
+    def next(self) -> None:
+        #print(self)
+        if self.remaining <= 0:
+            if self.remaining == 0:
+                self.complete = True
+            return
+        
+        self.cur_index += 1
+        self.remaining -= 1
+    
+    def prev(self) -> None:
+        '''This isnt really used, but might as well keep it.s'''
+        if self.remaining >= 0:
+            return
+        
+        self.cur_index -= 1
+        self.remaining += 1
+
+    def __txt_file_to_list(self, filepath: str) -> list[str]:
+        
+        with open(filepath) as file:
+            f_str = file.read()
+            token_list = f_str.split()
+
+        return token_list
+    
+    def __repr__(self) -> str:
+        return __name__ + f"({self.cur_index=},{self.remaining=},{self.get_current_word()=})"
